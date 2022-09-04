8aeb5851a7dcb1e7a38c622dd07c2b50257c8981
Blackdr3464 <dinhquyen03032000@gmail.com>
2022-09-04 09:08:47 +0700

    urlmongodb

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 8000;
-const urlMongoDB = `${process.env.MONGODB_URL}`;
-console.log(urlMongoDB);
 // connect mongoose
 mongoose
-    .connect(urlMongoDB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(
+        'mongodb+srv://blackdr:0979209251qQ@netflix.fyddlz4.mongodb.net/?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
     .then(() => {
         console.log('Connected db');
         app.listen(port, () => {
